fb48836139dbe1c794c9f6902c01f37fe3e61442
tkdisk <d.takaku49@gmail.com>
2025-05-06 17:36:10 +0900

    一般ユーザー認証機能テストの作成

---
+++ src/tests/Feature/RegisterTest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RegisterTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testNameIsRequiredOnRegistration()
+    {
+        $response = $this->post(route('register'), [
+            'name' => '',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $response->assertStatus(302)
+            ->assertSessionHasErrors(['name' => 'お名前を入力してください']);
+    }
+
+    public function testEmailIsRequiredOnRegistration()
+    {
+        $response = $this->post(route('register'), [
+            'name' => 'test',
+            'email' => '',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $response->assertStatus(302)
+            ->assertSessionHasErrors(['email' => 'メールアドレスを入力してください']);
+    }
+
+    public function testPasswordIsTooShortOnRegistration()
+    {
+        $response = $this->post(route('register'), [
+            'name' => 'test',
+            'email' => 'test@example.com',
+            'password' => 'pass',
+            'password_confirmation' => 'pass',
+        ]);
+
+        $response->assertStatus(302)
+            ->assertSessionHasErrors(['password' => 'パスワードは8文字以上で入力してください']);
+    }
+
+    public function testPasswordConfirmationMustMatchPasswordOnRegistration()
+    {
+        $response = $this->post(route('register'), [
+            'name' => 'test',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'passpass',
+        ]);
+
+        $response->assertStatus(302)
+            ->assertSessionHasErrors(['password' => 'パスワードと一致しません']);
+    }
+
+    public function testPasswordIsRequiredOnRegistration()
+    {
+        $response = $this->post(route('register'), [
+            'name' => 'test',
+            'email' => 'test@example.com',
+            'password' => '',
+            'password_confirmation' => 'password',
+        ]);
+
+        $response->assertStatus(302)
+            ->assertSessionHasErrors(['password' => 'パスワードを入力してください']);
+    }
+
+    public function testSuccessfulRegistrationPersistsUserRecord()
+    {
+        $response = $this->post(route('register'), [
+            'name' => 'test',
+            'email' => 'test@example.com',
+            'password' => 'password',
+            'password_confirmation' => 'password',
+        ]);
+
+        $response->assertSessionHasNoErrors();
+
+        $this->assertDatabaseHas('users', [
+            'name' => 'test',
+            'email' => 'test@example.com',
+        ]);
+    }
+}
